5093f6f3509def65d9b1301f15f76fcdf3e0ab09
kostiago <tiago_pb-@hotmail.com>
2025-05-30 10:20:55 -0300

    core: adding the initial H2 settings

---
+++ api.cliniq/src/test/java/com/kostiago/api/cliniq/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kostiago.api.cliniq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
